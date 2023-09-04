7d3fb0cc527847ed4c6e162626de5bd651a86e8e
Parham Safaiyan <parham.safaiyann@gmail.com>
2023-09-04 20:11:23 +0430

    (fix) added routes for find and create.

--- routes/users.php
+++ routes/users.php
@@ -11,10 +11,28 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/users', function (Request $request, Response $response) {
-    $a = new DB('sqlite:slim-chatroom.db');
-    $a->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $user = new User($a);
+    $db = new DB('sqlite:slim-chatroom.db');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $user = new User($db);
     $users = $user->getAll('users');
     $response->getBody()->write(json_encode($users));
-    return $response;
+    return $response->withHeader('Content-Type', 'application/json');
+});
+$app->post('/users', function (Request $request, Response $response) {
+    $inputs = $request->getParsedBody();
+    $db = new DB('sqlite:slim-chatroom.db');
+    $user = new User($db);
+    $users = $user->create('users', [
+        'display_name' => $inputs['username'],
+        'username' => $inputs['display_name']
+    ]);
+    $response->getBody()->write(json_encode($users));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+$app->get('/users/{id}', function (Request $request, Response $response, array $args) {
+    $db = new DB('sqlite:slim-chatroom.db');
+    $user = new User($db);
+    $users = $user->find('users', $args['id']);
+    $response->getBody()->write(json_encode($users));
+    return $response->withHeader('Content-Type', 'application/json');
 });
