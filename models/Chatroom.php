034b0755ab758584fae65a21f9997fa0f041f296
Parham Safaiyan <parham.safaiyann@gmail.com>
2023-09-05 21:38:41 +0430

    (fix) added two relations to chatroom and user models.

--- models/Chatroom.php
+++ models/Chatroom.php
@@ -5,5 +5,23 @@ use models\BaseModel;
 
 class Chatroom extends BaseModel
 {
+    public function users($chatroom)
+    {
+        try{
+            $sql = "
+                     SELECT users.display_name,users.username
+                     FROM chatroom_user
+                     JOIN users ON users.id = chatroom_user.user_id
+                     WHERE chatroom_user.chatroom_id = (:id);
+                    ";
+            $stmt = $this->db->prepare($sql);
+            $id = $chatroom->id;
+            $stmt->bindParam(':id',$id,\PDO::PARAM_INT);
+            $stmt->execute();
+            return $stmt->fetchAll();
+        }catch (\PDOException $e){
+            return $e->getMessage();
+        }
+    }
 
 }
--- models/User.php
+++ models/User.php
@@ -1,8 +1,27 @@
 <?php
 
 namespace models;
-require __DIR__.'/BaseModel.php';
+require __DIR__ . '/BaseModel.php';
+
 class User extends BaseModel
 {
+    public function chatrooms($user)
+    {
+        try{
+            $sql = "
+                     SELECT chatrooms.name
+                     FROM chatroom_user
+                     JOIN chatrooms  ON chatrooms.id = chatroom_user.chatroom_id
+                     WHERE chatroom_user.user_id = (:id);
+                    ";
+            $stmt = $this->db->prepare($sql);
+            $id = $user->id;
+            $stmt->bindParam(':id',$id,\PDO::PARAM_INT);
+            $stmt->execute();
+            return $stmt->fetchAll();
+        }catch (\PDOException $e){
+            return $e->getMessage();
+        }
 
+    }
 }
