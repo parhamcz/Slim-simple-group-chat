0e7c431513946df6c771fd3e64c8f763fa43eade
Parham Safaiyan <parham.safaiyann@gmail.com>
2023-09-06 00:10:04 +0430

    (fix) added join method to chatroom so users can join.

--- models/Chatroom.php
+++ models/Chatroom.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace models;
+
 use models\BaseModel;
 
 class Chatroom extends BaseModel
 {
     public function users($chatroom)
     {
-        try{
+        try {
             $sql = "
                      SELECT users.display_name,users.username
                      FROM chatroom_user
@@ -16,12 +17,26 @@ class Chatroom extends BaseModel
                     ";
             $stmt = $this->db->prepare($sql);
             $id = $chatroom->id;
-            $stmt->bindParam(':id',$id,\PDO::PARAM_INT);
+            $stmt->bindParam(':id', $id, \PDO::PARAM_INT);
             $stmt->execute();
             return $stmt->fetchAll();
+        } catch (\PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function join($chatroom, $user)
+    {
+        try{
+            $sql = "INSERT INTO chatroom_user VALUES (:chatroom_id,:user_id)";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':chatroom_id',$chatroom->id,\PDO::PARAM_INT);
+            $stmt->bindParam(':user_id',$user->id,\PDO::PARAM_INT);
+            return $stmt->execute();
         }catch (\PDOException $e){
             return $e->getMessage();
         }
+
     }
 
 }
