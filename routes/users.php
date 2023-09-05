e37ab646a5d4a3d27c9bc3208e32ad96139d7547
Parham Safaiyan <parham.safaiyann@gmail.com>
2023-09-06 01:03:22 +0430

    (fix*) binded the Controllers to routes.

--- routes/users.php
+++ routes/users.php
@@ -1,48 +1,17 @@
 <?php
 
-use database\DB;
-use models\User;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Controllers\User\UserController;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/users', function (Request $request, Response $response) {
-    $db = new DB('sqlite:slim-chatroom.db');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $user = new User($db);
-    $users = $user->getAll('users');
-    $response->getBody()->write(json_encode($users));
-    return $response->withHeader('Content-Type', 'application/json');
-});
-$app->post('/users', function (Request $request, Response $response) {
-    $inputs = $request->getParsedBody();
-    $db = new DB('sqlite:slim-chatroom.db');
-    $user = new User($db);
-    $users = $user->create('users', [
-        'display_name' => $inputs['username'],
-        'username' => $inputs['display_name']
-    ]);
-    $response->getBody()->write(json_encode($users));
-    return $response->withHeader('Content-Type', 'application/json');
-});
-$app->get('/users/{id}', function (Request $request, Response $response, array $args) {
-    $db = new DB('sqlite:slim-chatroom.db');
-    $user = new User($db);
-    $users = $user->find('users', $args['id']);
-    $response->getBody()->write(json_encode($users));
-    return $response->withHeader('Content-Type', 'application/json');
-});
-$app->get('/users/{id}/chatrooms', function (Request $request, Response $response, array $args) {
-    $db = new DB('sqlite:slim-chatroom.db');
-    $user = new User($db);
-    $result = $user->find('users', $args['id']);
-    $chatrooms = $user->chatrooms($result);
-    $response->getBody()->write(json_encode($chatrooms));
-    return $response->withHeader('Content-Type', 'application/json');
+$app->group('/users', function ($app) {
+    $app->get('', [UserController::class, 'all_users']);
+    $app->post('', [UserController::class, 'create']);
+    $app->get('/your-chatrooms', [UserController::class,'your_chatrooms']);
+    $app->get('/{id}',[UserController::class,'show']);
 });
 
 
