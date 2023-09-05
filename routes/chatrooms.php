7fe8c58dd91d2e67a9eab4377df6f62184478127
Parham Safaiyan <parham.safaiyann@gmail.com>
2023-09-06 02:03:42 +0430

    (fix) bounded chatroomController methods to chatroom routes.

--- routes/chatrooms.php
+++ routes/chatrooms.php
@@ -1,54 +1,16 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-use models\Chatroom;
-use database\DB;
+use Controllers\Chatroom\ChatroomController;
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../models/Chatroom.php';
 
 $app = AppFactory::create();
-$app->group('/chatrooms',function ($app){
-    $app->get('', function (Request $request, Response $response) {
-        $db = new DB('sqlite:slim-chatroom.db');
-        $result =new Chatroom($db);
-        $response->getBody()->write(json_encode($result->getAll('chatrooms')));
-        return $response->withHeader('Content-Type','application/json');
-    });
-    $app->post('', function (Request $request, Response $response) {
-        $inputs = $request->getParsedBody();
-        $db = new DB('sqlite:slim-chatroom.db');
-        $chatroom = new Chatroom($db);
-        $result = $chatroom->create('chatrooms', [
-            'name' => $inputs['name']
-        ]);
-        $response->getBody()->write(json_encode($result));
-        return $response->withHeader('Content-Type', 'application/json');
-    });
-    $app->get('/{id}', function (Request $request, Response $response, array $args) {
-        $db = new DB('sqlite:slim-chatroom.db');
-        $chatroom =new Chatroom($db);
-        $result = $chatroom->find('chatrooms',$args['id']);
-        $response->getBody()->write(json_encode($result));
-        return $response->withHeader('Content-Type','application/json');
-    });
-    $app->get('/{id}/users', function (Request $request, Response $response, array $args) {
-        $db = new DB('sqlite:slim-chatroom.db');
-        $user = new Chatroom($db);
-        $result = $user->find('chatrooms', $args['id']);
-        $chatrooms = $user->users($result);
-        $response->getBody()->write(json_encode($chatrooms));
-        return $response->withHeader('Content-Type', 'application/json');
-    });
-    $app->get('/{id}/join', function (Request $request, Response $response, array $args) {
-        $db = new DB('sqlite:slim-chatroom.db');
-        $user = new Chatroom($db);
-        $result = $user->find('chatrooms', $args['id']);
-        $chatrooms = $user->users($result);
-        $response->getBody()->write(json_encode($chatrooms));
-        return $response->withHeader('Content-Type', 'application/json');
-    });
+
+$app->group('/chatrooms', function ($app) {
+    $app->get('', [ChatroomController::class, 'all_chatrooms']);
+    $app->post('', [ChatroomController::class, 'create']);
+    $app->get('/{id}', [ChatroomController::class, 'show']);
+    $app->get('/{id}/join', [ChatroomController::class, 'join']);
 });
 
