8b8338eaa12bc450c127cb5452050bd7c3ea7a41
Parham Safaiyan <parham.safaiyann@gmail.com>
2023-09-06 01:04:48 +0430

    (fix) added namespaces.

--- Controllers/Chatroom/ChatroomController.php
+++ Controllers/Chatroom/ChatroomController.php
@@ -2,6 +2,14 @@
 
 namespace Controllers\Chatroom;
 
+use Controllers\Controller;
+use models\Chatroom;
+use database\DB;
+use PDO;
+use models\User;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
 class ChatroomController extends \Controllers\Controller
 {
 
