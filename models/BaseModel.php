1ca65f02ac8096643494a25090bd8cef494fc8bb
Parham Safaiyan <parham.safaiyann@gmail.com>
2023-09-06 20:34:10 +0430

    (fix) added delete method.

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -16,42 +16,55 @@ class BaseModel
 
     public function getAll($table)
     {
-            $stmt = $this->db->query("SELECT * FROM $table");
-            return $stmt->fetchAll();
+        $stmt = $this->db->query("SELECT * FROM $table");
+        return $stmt->fetchAll();
     }
 
     public function create(string $table, array $values)
     {
-            $fields = implode(', ', array_keys($values));
-            $placeholders = ':' . implode(', :', array_keys($values));
+        $fields = implode(', ', array_keys($values));
+        $placeholders = ':' . implode(', :', array_keys($values));
 
-            $sql = "INSERT INTO $table ($fields) VALUES ($placeholders)";
+        $sql = "INSERT INTO $table ($fields) VALUES ($placeholders)";
 
-            $stmt = $this->db->prepare($sql);
+        $stmt = $this->db->prepare($sql);
 
-            foreach ($values as $key => $value) {
-                $stmt->bindValue(':' . $key, $value);
-            }
+        foreach ($values as $key => $value) {
+            $stmt->bindValue(':' . $key, $value);
+        }
 
-            $stmt->execute();
-            return $values;
+        $stmt->execute();
+        return $values;
     }
 
     public function find($table, $id)
     {
-            $sql = "SELECT * FROM $table WHERE id = :id";
-            $stmt = $this->db->prepare($sql);
-            $stmt->bindValue(':id', $id);
-            $stmt->execute();
-            return $stmt->fetch();
+        $sql = "SELECT * FROM $table WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+        $data = $stmt->fetch();
+        if (empty($data)) {
+            return false;
+        }
+        return $data;
     }
 
     public function findByCol(string $table, string $field, string|int $value)
     {
-            $sql = "SELECT * FROM $table WHERE $field = :username";
-            $stmt = $this->db->prepare($sql);
-            $stmt->bindValue(':username', $value);
-            $stmt->execute();
-            return $stmt->fetch();
+        $sql = "SELECT * FROM $table WHERE $field = :username";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':username', $value);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+
+    public function delete($table, $id)
+    {
+        $sql = "DELETE FROM $table WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+        return $stmt->rowCount();
     }
 }
