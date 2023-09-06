efa458822d7ca5eb17e27b3328318f0ceb778ebe
Parham Safaiyan <parham.safaiyann@gmail.com>
2023-09-06 21:58:14 +0430

    (fix) added destroy route.

--- routes/chatrooms.php
+++ routes/chatrooms.php
@@ -16,6 +16,7 @@ $app->group('/chatrooms', function (RouteCollectorProxy $group) {
     $group->get('/{id}', [ChatroomController::class, 'show']);
     $group->get('/{id}/join', [ChatroomController::class, 'join']);
     $group->get('/{id}/leave', [ChatroomController::class, 'leave']);
+    $group->get('/{id}/destroy', [ChatroomController::class, 'destroy']);
     $group->get('/{chatroom_id}/user/{user_id}/set-admin', [ChatroomController::class, 'set_admin']);
 });
 
