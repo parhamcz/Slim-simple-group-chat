11c82efdfad3a3482fd9864cd7d5e484685cbcfe
Parham Safaiyan <parham.safaiyann@gmail.com>
2023-09-06 16:48:14 +0430

    (fix*) fixed join and leave method, user can only do them once.

--- models/Chatroom.php
+++ models/Chatroom.php
@@ -27,16 +27,42 @@ class Chatroom extends BaseModel
 
     public function join($chatroom, $user)
     {
-        try{
-            $sql = "INSERT INTO chatroom_user VALUES (:user_id,:chatroom_id)";
+        try {
+            $sql = "SELECT COUNT(*) as count FROM chatroom_user WHERE user_id = :user_id AND chatroom_id = :chatroom_id";
             $stmt = $this->db->prepare($sql);
-            $stmt->bindParam(':chatroom_id',$chatroom->id,\PDO::PARAM_INT);
-            $stmt->bindParam(':user_id',$user->id,\PDO::PARAM_INT);
-            return $stmt->execute();
-        }catch (\PDOException $e){
+            $stmt->bindParam(':user_id', $user->id, \PDO::PARAM_INT);
+            $stmt->bindParam(':chatroom_id', $chatroom->id, \PDO::PARAM_INT);
+            $stmt->execute();
+
+            $result = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+            if ($result['count'] == 0) {
+                // User is not in the chatroom, so insert them
+                $sql = "INSERT INTO chatroom_user (user_id, chatroom_id) VALUES (:user_id, :chatroom_id)";
+                $stmt = $this->db->prepare($sql);
+                $stmt->bindParam(':user_id', $user->id, \PDO::PARAM_INT);
+                $stmt->bindParam(':chatroom_id', $chatroom->id, \PDO::PARAM_INT);
+                $stmt->execute();
+                return true;
+            }
+            return false;
+        } catch (\PDOException $e) {
             return $e->getMessage();
         }
+    }
 
+    public function leave($chatroom, $user)
+    {
+        try {
+            $sql = "DELETE FROM chatroom_user WHERE user_id = :user_id AND chatroom_id = :chatroom_id";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':chatroom_id', $chatroom->id, \PDO::PARAM_INT);
+            $stmt->bindParam(':user_id', $user->id, \PDO::PARAM_INT);
+            $stmt->execute();
+            return $stmt->rowCount();
+        } catch (\PDOException $e) {
+            return $e->getMessage();
+        }
     }
 
 }
