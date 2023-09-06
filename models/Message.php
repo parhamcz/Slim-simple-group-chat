4463ec5f69bedaaf2e4abaf1fb00c15510bcf2ce
Parham Safaiyan <parham.safaiyann@gmail.com>
2023-09-06 18:55:37 +0430

    (fix) added send method.

--- models/Message.php
+++ models/Message.php
@@ -2,9 +2,14 @@
 
 namespace models;
 
-use database\DB;
-
-class Message extends DB
+class Message extends BaseModel
 {
-
+    public function send($user, $message, $chatroom)
+    {
+            return $this->create('chatroom_messages',[
+                'user_id' => $user->id,
+                'chatroom_id' => $chatroom->id,
+                'text' => $message,
+            ]);
+    }
 }
