6cea249f747a00ea96cd8c50f5a9f01423273a5a
Parham Safaiyan <parham.safaiyann@gmail.com>
2023-09-06 21:47:31 +0430

    (fix) used $group.

--- routes/messages.php
+++ routes/messages.php
@@ -2,12 +2,14 @@
 use Slim\Factory\AppFactory;
 use Controllers\Chatroom\MessageController;
 use Middlewares\Authentication;
+use Slim\Routing\RouteCollectorProxy;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 $app->add(new Authentication());
 
-$app->group('/messages', function ($app) {
-    $app->get('/chatroom/{id}', [MessageController::class, 'all_messages']);
-    $app->post('/chatroom/{id}/send', [MessageController::class, 'send']);
+$app->group('/messages', function (RouteCollectorProxy $group) {
+    $group->get('/chatroom/{id}', [MessageController::class, 'all_messages']);
+    $group->post('/chatroom/{id}/send', [MessageController::class, 'send']);
 });
--- routes/users.php
+++ routes/users.php
@@ -3,16 +3,18 @@
 use Slim\Factory\AppFactory;
 use Controllers\User\UserController;
 use Middlewares\Authentication;
+use Slim\Routing\RouteCollectorProxy;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 $app->add(new Authentication());
 
-$app->group('/users', function ($app) {
-    $app->get('', [UserController::class, 'all_users']);
-    $app->post('', [UserController::class, 'create']);
-    $app->get('/your-chatrooms', [UserController::class,'your_chatrooms']);
-    $app->get('/{id}',[UserController::class,'show']);
+$app->group('/users', function (RouteCollectorProxy $group) {
+    $group->get('', [UserController::class, 'all_users']);
+    $group->post('', [UserController::class, 'create']);
+    $group->get('/your-chatrooms', [UserController::class,'your_chatrooms']);
+    $group->get('/{id}',[UserController::class,'show']);
 });
 
 
