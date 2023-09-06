46f749aacf32763342c64057f8e9df150e13050f
Parham Safaiyan <parham.safaiyann@gmail.com>
2023-09-06 20:37:54 +0430

    (fix) fixed result method.

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -6,7 +6,12 @@ class Controller
 {
     public function result(bool $status, string $message, $data, $code = 200): array
     {
-
+        return [
+            'status' => $status,
+            'message' => $message,
+            'data' => $data,
+            'status code' => $code
+        ];
     }
 
     public function notFound(string $message)
