62c329675625c93a304a508d412ce2ff89b02f01
Parham Safaiyan <parham.safaiyann@gmail.com>
2023-09-06 18:56:13 +0430

    (fix) added routes.

--- routes/messages.php
+++ routes/messages.php
@@ -1,1 +1,11 @@
 <?php
+use Slim\Factory\AppFactory;
+use Controllers\Chatroom\MessageController;
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->group('/messages', function ($app) {
+    $app->get('/chatroom/{id}', [MessageController::class, 'all_messages']);
+    $app->post('/chatroom/{id}/send', [MessageController::class, 'send']);
+});
