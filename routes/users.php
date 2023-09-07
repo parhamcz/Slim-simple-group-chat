45098a6d375d7a9bd490771c8ec7296447d09dec
Parham <parham.safaiyan@gmail.com>
2023-09-07 21:19:03 +0330

    (fix) excluded creating user from auth middleware.

--- routes/users.php
+++ routes/users.php
@@ -8,13 +8,14 @@ use Slim\Routing\RouteCollectorProxy;
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
-$app->add(new Authentication());
 
 $app->group('/users', function (RouteCollectorProxy $group) {
-    $group->get('', [UserController::class, 'all_users']);
     $group->post('', [UserController::class, 'create']);
-    $group->get('/your-chatrooms', [UserController::class,'your_chatrooms']);
-    $group->get('/{id}',[UserController::class,'show']);
+    $group->group('',function (RouteCollectorProxy $group){
+        $group->get('', [UserController::class, 'all_users']);
+        $group->get('/your-chatrooms', [UserController::class,'your_chatrooms']);
+        $group->get('/{id}',[UserController::class,'show']);
+    })->addMiddleware(new Authentication());
 });
 
 
