d0404f7ebf8d7f02d1c49baf8a4060a07074a82c
Parham <parham.safaiyan@gmail.com>
2023-09-08 15:36:08 +0330

    (add) added ErrorHandle Middleware too all routes.

--- public/index.php
+++ public/index.php
@@ -3,11 +3,12 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Middlewares\ErrorHandle;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
-
+$app->add(new ErrorHandle());
 $app->get('/', function (Request $request, Response $response) {
     $response->getBody()->write(json_encode(['message' => 'hello world!']));
     return $response->withHeader('Content-Type', 'application/json');
